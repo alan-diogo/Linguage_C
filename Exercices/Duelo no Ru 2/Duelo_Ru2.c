abf7fad00cc237e6f93a3a9223d9db7f7a71c796
alan-diogo <alandiogo9@gmail.com>
2025-03-03 18:09:18 -0300

    Final Duelo 2

--- Exercices/Duelo no Ru 2/Duelo_Ru2.c
+++ Exercices/Duelo no Ru 2/Duelo_Ru2.c
@@ -1,105 +1,134 @@
 #include <stdio.h>
 #include <stdbool.h>
+
 int primeiro_digito(int num)
 {
   if (num >= 10)
   {
-    num /= 10;
+    return primeiro_digito(num / 10);
   }
-  else
-  {
-    return num;
-  }
-
-  return primeiro_digito(num);
+  return num;
 }
+
 int coprimo(int a, int b, int count)
 {
-
   if (count > a || count > b)
-    return true;
+    return 1;
   if (a % count == 0 && b % count == 0)
-    return false;
-
+    return 0;
   return coprimo(a, b, count + 1);
 }
+
 int fibo(int valor, int a, int b)
 {
   if (a == valor)
-    return true;
+    return 1;
   if (a > valor)
-    return false;
+    return 0;
   return fibo(valor, b, a + b);
 }
 
 typedef struct Card
 {
-  double atk;
-  double def;
+  int def;
+  int atk;
 } Card;
+
 typedef struct friends
 {
   int confidence;
   char hungry;
 } Friends;
 
-int ReadFriends(int numFriends, int life, int countBonus)
+int Battle(Card ekul, Card luke)
 {
-
-  if (numFriends == 0)
+  // Ekul atk
+  luke.def -= ekul.atk;
+  if (luke.def <= 0)
   {
-    return countBonus;
+    printf("Ekul venceu o round!\nVida restante: %d\n\n", ekul.def);
+    return 1;
   }
+
+  // Luke atk
+  ekul.def -= luke.atk;
+  if (ekul.def <= 0)
+  {
+    printf("Luke venceu o round!\nVida restante: %d\n\n", luke.def);
+    return 0;
+  }
+
+  return Battle(ekul, luke);
+}
+
+int ReadFriends(int numFriends, int life, int bonus)
+{
+  if (numFriends == 0)
+    return bonus;
 
   Friends friend;
   scanf("%d %c", &friend.confidence, &friend.hungry);
 
-  if (fibo(friend.confidence, 0, 1) && friend.hungry == 'F')
+  if (friend.hungry == 'F')
   {
-    countBonus += friend.confidence % 100;
+    if (fibo(friend.confidence, 0, 1))
+    {
+      bonus += friend.confidence % 100;
+    }
   }
-  else if (coprimo(friend.confidence, life, 2) && friend.hungry == 'C')
+  else if (friend.hungry == 'C')
   {
-    countBonus += primeiro_digito(friend.confidence) * 10;
+    if (coprimo(friend.confidence, life, 2))
+    {
+      bonus += primeiro_digito(friend.confidence) * 10;
+    }
   }
 
-  ReadFriends(--numFriends, life, countBonus);
+  return ReadFriends(numFriends - 1, life, bonus);
 }
-void CountRounds(int NRounds)
+
+void CountRounds(int rounds, int ekul_wins, int luke_wins)
 {
-  if (NRounds == 0)
+  if (rounds == 0)
   {
+    if (ekul_wins > luke_wins)
+    {
+      printf("Ekul liberou a entrada do RU!\n");
+    }
+    else
+    {
+      printf("Luke concluiu seu plano maligno!\n");
+    }
+    return;
   }
-  Card ekul;
-  Card luke;
-  int numFriends;
-  scanf("%lf %lf", &ekul.atk, &ekul.def);
-  scanf("%lf %lf", &luke.atk, &luke.def);
-  scanf("%d", &numFriends);
-  double BonusDef = ReadFriends(numFriends, ekul.def, 0);
-  double BonusAtk = BonusDef / 2;
-  ekul.atk += BonusAtk;
+
+  Card ekul, luke;
+  int M;
+
+  scanf("%d %d", &ekul.def, &ekul.atk);
+  scanf("%d %d", &luke.def, &luke.atk);
+  scanf("%d", &M);
+
+  int BonusDef = ReadFriends(M, ekul.def, 0);
+  int BonusAtk = BonusDef / 2;
+
   ekul.def += BonusDef;
+  ekul.atk += BonusAtk;
 
-  Beattle(ekul, luke);
+  int result = Battle(ekul, luke);
 
-  Rounds(--NRounds);
-}
+  if (result == 1)
+    ekul_wins++;
+  else
+    luke_wins++;
 
-int Battle(Card ekull, Card luke)
-{
-  luke.def -= ekull.atk;
-  if (luke.def >= 0)
-  {
-    return 1;
-  }
+  CountRounds(rounds - 1, ekul_wins, luke_wins);
 }
 
 int main()
 {
-
-  int rounds;
-  scanf("%d", &rounds);
-
-  CountRounds(rounds);
+  int N;
+  scanf("%d", &N);
+  CountRounds(N, 0, 0);
+  return 0;
 }
